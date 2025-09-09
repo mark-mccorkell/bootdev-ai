d03d75c5b9efc684783cd38bdf08dc2cb6517f6b
Mark McCorkell <djmmccorkell@gmail.com>
2025-09-09 21:53:30 +0100

    Add CLI user prompt

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-    # Get the Gemini API key from .env
+    # Load environment from .env
     load_dotenv()
+
+    # Check for prompt in CLI args
+    args = sys.argv[1:]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+
+    # Fetch API key from .env
     api_key = os.environ.get("GEMINI_API_KEY")
 
     # Create a new instance of a Gemini client
@@ -13,7 +26,7 @@ def main():
     # Get a response from Gemini using a hard-coded model and prompt
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=user_prompt,
     )
 
     # Print the response

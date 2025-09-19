6335ee462782b0843c961d7ac67a5101b7af8908
Mark McCorkell <djmmccorkell@gmail.com>
2025-09-19 23:01:31 +0100

    Add --verbose flag

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ def main():
     # Load environment from .env
     load_dotenv()
 
+    # Check for --verbose argument
+    verbose = "--verbose" in sys.argv
+
     # Check for prompt in CLI args
-    args = sys.argv[1:]
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    # Print usage if no args provided
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
     
+   
     # Fetch API key from .env
     api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -25,16 +34,20 @@ def main():
     # Combine all arguments into a single user prompt
     user_prompt = " ".join(args)
 
+    # If --verbose then print user prompt
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
     # Create a list of messages for future use in conversations
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, verbose)
 
 
 # Print out a response from Gemini passing in conversation history in 'messages'
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
 
     # Get a response from Gemini using a hard-coded model and user prompt
     response = client.models.generate_content(
@@ -43,9 +56,9 @@ def generate_content(client, messages):
     )
 
     # Print the response
-
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if (verbose):
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
